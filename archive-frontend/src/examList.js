2553a5519d3dcbe3d9fa3fa8ab8dd269a11bf172
yeonsu <jack0810@kookmin.ac.kr>
2023-12-07 05:28:27 +0900

    feat: sync with local

--- archive-frontend/src/examList.js
+++ archive-frontend/src/examList.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
+import "./examList.css";
 
 export default function ExamList() {
   const { category } = useParams();
--- archive-frontend/src/home.js
+++ archive-frontend/src/home.js
@@ -1,5 +1,17 @@
 import { Link } from "react-router-dom";
 
 export default function Home() {
-  return <Link to={"/examList/computer_architecture"}>컴퓨터 구조</Link>;
+  const textStyle = {
+    fontSize: "100px", // 텍스트 크기 조절
+    fontWeight: "bold", // 필요에 따라 다른 스타일 속성도 추가할 수 있습니다.
+    color: "black",
+  };
+
+  return (
+    <div className="App">
+      <p style={textStyle}>Software Archive</p>
+      <br />
+      <Link to={"/examList/computer_architecture"}>컴퓨터 구조</Link>
+    </div>
+  );
 }
